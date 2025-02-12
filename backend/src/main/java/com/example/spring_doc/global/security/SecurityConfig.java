9167e908539aefcb9478e98eb3e7a436e09285f6
dlfjsld1 <ltk900216@gmail.com>
2025-02-12 11:44:07 +0900

    url filter changed.  blacklist to whitelist

--- backend/src/main/java/com/example/spring_doc/global/security/SecurityConfig.java
+++ backend/src/main/java/com/example/spring_doc/global/security/SecurityConfig.java
@@ -32,10 +32,10 @@ public class SecurityConfig {
                                 .permitAll()
                                 .requestMatchers("/api/v1/posts/statistics")
                                 .hasRole("ADMIN")
-                                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**")
-                                .permitAll()
-                                .anyRequest()
+                                .requestMatchers("/api/*/**")
                                 .authenticated()
+                                .anyRequest()
+                                .permitAll()
                 )
                 .headers((headers) -> headers
                         .addHeaderWriter(new XFrameOptionsHeaderWriter(
